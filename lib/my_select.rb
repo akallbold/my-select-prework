d45c8da9364407f72a5a6756bf9ab0be2ab6b878
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-15 17:28:29 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ if collection
+  counter = 0
+  output=[]
+
+  while counter<collection.size do 
+     if yield(collection[counter])==true
+       output << yield(collection[counter])
+     end
+     counter+=1
+  end
+ end
 end
